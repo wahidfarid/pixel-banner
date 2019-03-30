44020310579f08b64ca6dece69c35833b7321e63
Wahid Magdy <wahidmagdy@gmail.com>
2019-03-30 21:14:30 +0200

    Create and draw on canvas element

--- pixel-banner.js
+++ pixel-banner.js
@@ -15,7 +15,36 @@
 
 	// Draw the pixels
 	function drawPixels(src){
-		src.target_div.style.background = "red";
+		
+		// Get target size
+		src.target_width = src.target_div.offsetWidth;
+		src.target_height = src.target_div.offsetHeight;
+
+		// Create Canvas Element
+		src.canvas = src.target_div.appendChild(initializeCanvas(src.target_width, src.target_height));
+
+		// Draw on Canvas
+		drawOnCanvas(src.canvas);
+
+		src.target_div.style.background = "yellow";
+	}
+
+	//Create Canvas Element
+	function initializeCanvas(width, height){
+		var canvas = document.createElement("canvas");
+		canvas.style.width = width;
+		canvas.style.height = height;
+		canvas.style.position = "absolute";
+
+		return canvas;
+	}
+
+	// Draw on Canvas
+	function drawOnCanvas(c){
+		var ctx = c.getContext("2d");
+		ctx.beginPath();
+		ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+		ctx.stroke();
 	}
 
     // make accessible globally
