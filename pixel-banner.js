154e92f4399a511137f9127f72eb7183865be11b
Wahid Magdy <wahidmagdy@gmail.com>
2019-03-30 22:52:26 +0200

    refactor drawing loop

--- pixel-banner.js
+++ pixel-banner.js
@@ -11,19 +11,23 @@
     // Attach our defaults for plugin to the plugin itself
     PixelBanner.defaults = {
 		target: "body",
+		static: false,
 		pixel_options: {
 			size: {width: 30, height: 30},
 			color_pallete: ["#6d8891", "#dfe6ea", "#dacdbe", "#cda20b", "#6b3603"],
 			probability: {x_axis: "always", y_axis: "always"}
-
 		}
     }
 	
 	// Configuration
 	function configureOptions(src, opts){
+
+		// Merge user given options with default
 		src.options = mergeDeep(PixelBanner.defaults , opts);
 		src.target_div = document.querySelector(src.options.target);
 
+		// Dynamic runner
+		//if(src.options.static)
 
 		// Probability method lookups
 		if(src.options.pixel_options.probability.x_axis == "always")
@@ -67,23 +71,29 @@
 		var ctx = c.getContext("2d");
 		var w = opt.size.width;
 		var h = opt.size.height;
-		var pallete = opt.color_pallete;
-		
-		ctx.imageSmoothingEnabled = false;
+		var pallete = opt.color_pallete;	
 
 		for(var x = 0; x < c.width; x+= w){
 			for(var y = 0; y < c.height; y+= h){
-				
-				console.log(opt);
-				// Skip based on probability
-				if(Math.random() > opt.probability.x_axis(x, c.width) * opt.probability.y_axis(y, c.height))
-					continue;
-				ctx.fillStyle = pallete[Math.floor(Math.random() * pallete.length)];
-				ctx.fillRect(x, y, w, h);
+			  if(calculateProbability(opt.probability, x, y, c))
+				drawPixel(x, y, w, h, pallete, ctx);
 			}
 		}
 	}
-	
+
+	// Calculate Probability
+	function calculateProbability(prob, x, y, c){
+		// Skip based on probability
+		if(Math.random() > prob.x_axis(x, c.width) * prob.y_axis(y, c.height))
+			return false;
+		return true;
+	}
+	// Draw a single pixel
+	function drawPixel(x, y, w, h, pallete, ctx){
+		ctx.imageSmoothingEnabled = false;
+		ctx.fillStyle = pallete[Math.floor(Math.random() * pallete.length)];
+		ctx.fillRect(x, y, w, h);
+	}
 	// Probability Functions
 	function probabilityFromDistanceToCenter(position, length) {
 		return   1 - (Math.abs(position - (length/2))  / (length/2));
