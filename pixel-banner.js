a09633da9629e94a9d33b7e344817202eef3a863
Wahid Magdy <wahidmagdy@gmail.com>
2019-03-30 21:28:30 +0200

    Draw a single random pixel from a pallete

--- pixel-banner.js
+++ pixel-banner.js
@@ -10,7 +10,11 @@
 
     // Attach our defaults for plugin to the plugin itself
     PixelBanner.defaults = {
-		target: "body"
+		target: "body",
+		pixel_options: {
+			size: {width: 50, height: 50},
+			color_pallete: ["#6d8891", "#dfe6ea", "#dacdbe", "#cda20b", "#6b3603"]
+		}
     }
 
 	// Draw the pixels
@@ -24,27 +28,31 @@
 		src.canvas = src.target_div.appendChild(initializeCanvas(src.target_width, src.target_height));
 
 		// Draw on Canvas
-		drawOnCanvas(src.canvas);
+		drawOnCanvas(src.canvas, src.options.pixel_options);
 
-		src.target_div.style.background = "yellow";
 	}
 
 	//Create Canvas Element
 	function initializeCanvas(width, height){
 		var canvas = document.createElement("canvas");
-		canvas.style.width = width;
-		canvas.style.height = height;
+		canvas.width = width;
+		canvas.height = height;
 		canvas.style.position = "absolute";
 
 		return canvas;
 	}
 
 	// Draw on Canvas
-	function drawOnCanvas(c){
+	function drawOnCanvas(c, opt){
 		var ctx = c.getContext("2d");
-		ctx.beginPath();
-		ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-		ctx.stroke();
+		var w = opt.size.width;
+		var h = opt.size.height;
+		var pallete = opt.color_pallete;
+		
+		ctx.imageSmoothingEnabled = false;
+
+		ctx.fillStyle = pallete[Math.floor(Math.random() * pallete.length)];
+		ctx.fillRect(10, 10, w, h);
 	}
 
     // make accessible globally
